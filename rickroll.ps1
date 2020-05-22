ddc4e022c4522529dae0a0387521688d0a502cbe
Gideon Shaked <gideonshaked@gmail.com>
2020-05-22 16:07:42 -0400

    Create rickroll.ps1

---
+++ rickroll.ps1
@@ -0,0 +1,4 @@
+﻿# A Powershell script to open Microsoft Edge and play Rick Astley's "Never Gonna Give You Up"
+
+# In all of my tests, Edge autoplays videos. If it does not work, sue me.
+start microsoft-edge:https://www.youtube.com/watch?v=dQw4w9WgXcQ
