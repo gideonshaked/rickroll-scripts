e83c3d9f0cb00ce2f495904a1c9b5cd5e3bfbeaa
The-Kid-Gid <gideonshaked@gmail.com>
2020-06-03 15:16:04 -0400

    Added functionality to set volume to 100% for linux

--- rickroll.sh
+++ rickroll.sh
@@ -2,7 +2,6 @@
 # A script to open Mozilla Firefox to the URL of "Never Gonna Give You Up" by Rick Astley and play it
 
 firefox --new-tab https://www.youtube.com/watch?v=dQw4w9WgXcQ &
-
-sleep 5
-
+pactl set-sink-volume 0 +100%
+sleep 10
 xdotool search "Mozilla Firefox" key space
