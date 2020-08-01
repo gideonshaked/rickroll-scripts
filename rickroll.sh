522a56996e3249ecb18e663e21c6cdd611e21078
The-Kid-Gid <gideonshaked@gmail.com>
2020-08-01 13:33:52 -0400

    Remove volume control

--- rickroll.sh
+++ rickroll.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # A script to open Mozilla Firefox to the URL of "Never Gonna Give You Up" by Rick Astley and play it
 
-firefox --new-tab https://www.youtube.com/watch?v=dQw4w9WgXcQ &
-pactl set-sink-volume 0 +100%
+firefox --new-tab https://www.youtube.com/watch?v=dQw4w9WgXcQ
 sleep 10
 xdotool search "Mozilla Firefox" key space
